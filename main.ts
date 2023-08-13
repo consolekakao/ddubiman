935851637f291262de5947fe0d1d6de11d6f749e
consolekakao <console@kakao.com>
2023-08-13 16:27:13 +0900

    feat : add example theme color

--- main.ts
+++ main.ts
@@ -1,23 +1,46 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain, nativeTheme } from "electron";
 import isDev from "electron-is-dev";
 import path from "path";
 
 let window;
 
-app.on("ready", () => {
+const createWindow = () => {
   console.log("ready!");
   window = new BrowserWindow({
-    width: 800,
+    width: 1920,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
-
   window.loadURL(
     isDev
       ? "http://localhost:5173"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  // window.loadFile("index.html");
+};
+
+ipcMain.handle("dark-mode:toggle", () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = "light";
+  } else {
+    nativeTheme.themeSource = "dark";
+  }
+  return nativeTheme.shouldUseDarkColors;
+});
+
+ipcMain.handle("dark-mode:system", () => {
+  nativeTheme.themeSource = "system";
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
