97e2b1b77e3d49bb1593ae3aa06a2db0aed16479
consolekakao <console@kakao.com>
2023-08-13 13:58:56 +0900

    first init

---
+++ index.js
@@ -0,0 +1,17 @@
+// import { app, BrowserWindow } from "electron";
+const { app, BrowserWindow } = require("electron");
+
+let window;
+
+app.on("ready", () => {
+  console.log("ready!");
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  window.loadFile("index.html");
+});
